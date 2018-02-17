299f07dd99d10a40fb5216191a493907069c7b80
Marshall McDougall <marshall.mcdougall@gmail.com>
2018-02-17 14:02:10 -0800

    Commit before ejecting from react native

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View, Alert, Button } from 'react-native';
+import { StyleSheet, Text, View, Alert, Button, TextInput } from 'react-native';
+
+
+var PropTypes = require('prop-types');
+var dgram = require('dgram');
+console.log(dgram);
+console.log(dgram.createSocket())
+//console.log(socket);
+//socket.bind(8000);
 
 export default class App extends React.Component {
+
   render() {
     return (
       <View style={styles.container}>
+        
+        <TextInput
+        style={{height:40}}
+        placeholder="Enter your message here."
+        onChangeText={(text) => this.setState({text})}
+        />
+
         <Button
           onPress={() => {
-            Alert.alert('You tapped the button!');
+            Alert.alert(this.state.text);
           }}
-          title="Connect"/>
+          title="Send Message"/>
       </View>
     );
   }
